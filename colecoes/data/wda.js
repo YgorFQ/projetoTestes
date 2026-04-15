32253fd55ac3b35e67420f0c49b98bab25a011d7
Ygor Ferreira <142054957+YgorFQ@users.noreply.github.com>
2026-04-15 11:50:10 -0300

    [SenkoLib] create collection: wda

---
+++ colecoes/data/wda.js
@@ -0,0 +1,12 @@
+// @ts-nocheck
+ColLib.registerCollection({
+  slug:   'wda',
+  name:   'wda',
+  tags:   ['wda'],
+  author: 'wda',
+  color:  '#D4537E',
+});
+
+ColLib.registerLayout('wda', [
+
+]);
