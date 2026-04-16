873c014075d45fcf13ff585a0c924d1318aaec15
Ygor Ferreira <142054957+YgorFQ@users.noreply.github.com>
2026-04-16 12:26:16 -0300

    [SenkoLib] create collection: testesss

---
+++ colecoes/data/testesss.js
@@ -0,0 +1,12 @@
+// @ts-nocheck
+ColLib.registerCollection({
+  slug:   'testesss',
+  name:   'testesss',
+  tags:   [],
+  author: '',
+  color:  '',
+});
+
+ColLib.registerLayout('testesss', [
+
+]);
