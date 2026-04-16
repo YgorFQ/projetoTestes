f643dd0bd8bf6694d812b95b76fbbf60d7d7e8d8
Ygor Ferreira <142054957+YgorFQ@users.noreply.github.com>
2026-04-15 22:43:48 -0300

    [SenkoLib] create collection: awsssssss

---
+++ colecoes/data/awsssssss.js
@@ -0,0 +1,12 @@
+// @ts-nocheck
+ColLib.registerCollection({
+  slug:   'awsssssss',
+  name:   'awsssssss',
+  tags:   [],
+  author: '',
+  color:  '',
+});
+
+ColLib.registerLayout('awsssssss', [
+
+]);
