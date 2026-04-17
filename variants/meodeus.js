86ecbfcce97caa899111ae38243a5af2b47fc338
Ygor Ferreira <142054957+YgorFQ@users.noreply.github.com>
2026-04-17 10:35:33 -0300

    [SenkoLib] delete variants: meodeus

--- variants/meodeus.js
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-SenkoLib.registerVariant('meodeus', [
-/*@@@@Senko - sectiuon8888888 */
-  {
-    name: 'sectiuon8888888',
-    html: `cawrawdawdawd waw d aAEQDeawdac`,
-    css: `awdawdawddawdwd`,
-  },
-]);
