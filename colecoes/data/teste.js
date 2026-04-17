88045dfadf7bec456bd5a02b3ceece6418cdbcd1
Ygor Ferreira <142054957+YgorFQ@users.noreply.github.com>
2026-04-17 10:39:35 -0300

    [SenkoLib] create collection: teste

---
+++ colecoes/data/teste.js
@@ -0,0 +1,15 @@
+// @ts-nocheck
+/* ═══════════════════════════════════════════════════════════════════════
+   colecoes/data/teste.js — Coleção: teste
+
+   ATENÇÃO: Arquivo gerado pelo módulo GitHub do SenkoLib.
+   NÃO edite manualmente em produção.
+═══════════════════════════════════════════════════════════════════════ */
+ColLib.register({
+  slug:  'teste',
+  name:  'teste',
+  group: 'pedro',
+  tags:  ['Cavalo'],
+  layouts: [
+  ]
+});
