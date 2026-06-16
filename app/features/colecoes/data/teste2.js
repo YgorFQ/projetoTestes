7d4c8c00a47cd8c7491a68f7771475431fca1f1a
Ygor Ferreira <142054957+YgorFQ@users.noreply.github.com>
2026-06-16 07:43:32 -0300

    [Colecoes] create collection: teste2

---
+++ app/features/colecoes/data/teste2.js
@@ -0,0 +1,15 @@
+// @ts-nocheck
+/* ═══════════════════════════════════════════════════════════════════════
+   app/features/colecoes/data/teste2.js — Coleção: Teste2
+
+   ATENÇÃO: Arquivo gerado pelo modulo GitHub de Colecoes.
+   NÃO edite manualmente em produção.
+═══════════════════════════════════════════════════════════════════════ */
+ColLib.register({
+  slug:  'teste2',
+  name:  'Teste2',
+  group: 'ygor',
+  tags:  ['awdaw', 'dawdawd', 'awdawdwedawad'],
+  layouts: [
+  ]
+});
