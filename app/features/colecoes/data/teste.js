d9c4e2d13fb4c620661961cce5d615be95104dac
Ygor Ferreira <142054957+YgorFQ@users.noreply.github.com>
2026-06-16 07:33:00 -0300

    [Colecoes] create collection: teste

---
+++ app/features/colecoes/data/teste.js
@@ -0,0 +1,15 @@
+// @ts-nocheck
+/* ═══════════════════════════════════════════════════════════════════════
+   app/features/colecoes/data/teste.js — Coleção: Teste
+
+   ATENÇÃO: Arquivo gerado pelo modulo GitHub de Colecoes.
+   NÃO edite manualmente em produção.
+═══════════════════════════════════════════════════════════════════════ */
+ColLib.register({
+  slug:  'teste',
+  name:  'Teste',
+  group: 'egambling',
+  tags:  ['ser gamer', 'ser um jogador', 'tcs'],
+  layouts: [
+  ]
+});
