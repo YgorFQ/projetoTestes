1194806b0f36a003b6df8810b79b73e7ba17ec43
Ygor Ferreira <142054957+YgorFQ@users.noreply.github.com>
2026-07-08 08:57:00 -0300

    [SenkoLib] add layout file: teste-gamer-3

---
+++ app/features/biblioteca/data/layouts/teste-gamer-3.js
@@ -0,0 +1,12 @@
+// @ts-nocheck
+SenkoLib.registerLayout(
+SenkoLib.registerLayout(
+{
+    id: 'teste-gamer-3',
+    name: 'teste-gamer-3',
+    tags: ['sex', 'gamer'],
+    html: `hello world`,
+    css: `hello world`
+}
+);
+);
