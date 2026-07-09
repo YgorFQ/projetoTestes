35992df11c366d444f8658d33c60ef989574ef9d
Ygor Ferreira <142054957+YgorFQ@users.noreply.github.com>
2026-07-09 18:21:15 -0300

    [SenkoLib] delete variant file: teste32/section-94-1

--- app/features/biblioteca/data/variants/teste32/section-94-1.js
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-SenkoLib.registerVariantFile("teste32",
-{
-  id: "section-94-1",
-  name: "section-94-2",
-  html: `dawdawdadwdawdawdadwadwdawdw`,
-  css: `awdawdawdwadwdwadwad`
-}
-);
