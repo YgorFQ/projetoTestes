94edbe90d9f9e934b5eba7c9ef67bbd1d74af880
Ygor Ferreira <142054957+YgorFQ@users.noreply.github.com>
2026-07-09 18:17:32 -0300

    [SenkoLib] edit variant file: teste32/section-94-1

--- app/features/biblioteca/data/variants/teste32/section-94-1.js
+++ app/features/biblioteca/data/variants/teste32/section-94-1.js
@@ -2,8 +2,8 @@
 SenkoLib.registerVariantFile("teste32",
 {
   id: "section-94-1",
-  name: "section-94-1",
-  html: `dawdawdadwdawdawd`,
+  name: "section-94-2",
+  html: `dawdawdadwdawdawdadwadwdawdw`,
   css: `awdawdawdwadwdwadwad`
 }
 );
