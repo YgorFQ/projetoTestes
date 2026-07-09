f3694e4b5c44dc3fc310a96eb6ce242169082690
Ygor Ferreira <142054957+YgorFQ@users.noreply.github.com>
2026-07-09 18:09:11 -0300

    [SenkoLib] add layout file: teste32

---
+++ app/features/biblioteca/data/layouts/teste32.js
@@ -0,0 +1,10 @@
+// @ts-nocheck
+SenkoLib.registerLayout(
+{
+    id: 'teste32',
+    name: 'teste32',
+    tags: ['teste32', 'teste32', 'teste32caeu'],
+    html: `´teste32`,
+    css: `teste32`
+}
+);
