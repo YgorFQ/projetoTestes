f5bd12cb35b2cf6d11bd68fbc44706272f3e4301
Ygor Ferreira <142054957+YgorFQ@users.noreply.github.com>
2026-07-10 08:31:32 -0300

    [Colecoes] edit collection: carneiro-colecoes

--- app/features/colecoes/data/collections/carneiro-colecoes/collection.js
+++ app/features/colecoes/data/collections/carneiro-colecoes/collection.js
@@ -1,8 +1,8 @@
 // @ts-nocheck
 ColLib.registerCollection({
   slug:  'carneiro-colecoes',
-  name:  'Ygor Coleçõess',
-  group: 'selecao-francesa',
-  tags:  ['Responsivo', 'adaptativos', 'gamer'],
+  name:  'Ygor Coleções',
+  group: 'ygor',
+  tags:  ['Responsivo', 'adaptativos'],
   layouts: []
 });
