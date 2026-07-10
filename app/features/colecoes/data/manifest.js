8887893c789cf2086d14689dd2d9cb40a3627a68
Ygor Ferreira <142054957+YgorFQ@users.noreply.github.com>
2026-07-10 08:34:07 -0300

    [Colecoes] register collection file: teste

--- app/features/colecoes/data/manifest.js
+++ app/features/colecoes/data/manifest.js
@@ -232,6 +232,19 @@ window.SenkoColecoesManifest = {
           "name": "padrão 2"
         }
       ]
+    },
+    {
+      "file": "collections/teste/collection.js",
+      "slug": "teste",
+      "name": "Teste",
+      "group": "test",
+      "tags": [
+        "Teste",
+        "test",
+        "Teste"
+      ],
+      "layoutCount": 0,
+      "layouts": []
     }
   ]
 };
